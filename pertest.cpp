1ccd4e9c3ead99c7147d8ababe434c9034ace8b6
Bjørn Rustad <bjorn@rustad.me>
2015-01-17 15:53:49 +0100

    Define circle by radius in pertest

--- pertest.cpp
+++ pertest.cpp
@@ -6,15 +6,47 @@
 #include <opencv2/opencv.hpp>
 #include "neighborhood.hpp"
 
+#define PADDING 20
+
 using namespace std;
 using namespace cv;
 
-double per(Neighborhood &neigh) {
+double per(Neighborhood &neigh, double radius) {
 	double p = 0.0;
 
-	for (Neighborhood::iterator it = neigh.begin(); it != neigh.end(); ++it) {
-		Mat ee = (Mat_<double>(2, 1) << it->x, it->y);
-		p += norm(ee) * norm(ee) * it->dt / (2.0 * pow(ee.dot(ee), 3.0 / 2.0));
+	int center = radius + PADDING;
+	for (int i = 0; i < 2 * center; ++i) {
+		for (int j = 0; j < 2 * center; ++j) {
+			Neighborhood::iterator it;
+			for (it = neigh.begin(); it != neigh.end(); ++it) {
+				Mat ee = (Mat_<double>(2, 1) << it->x, it->y);
+
+				int x = j + it->x;
+				int y = i + it->y;
+
+				if (it->x < 0)
+					continue;
+				if (it->x == 0 && it->y > 0)
+					continue;
+
+				int rx = x - center;
+				int ry = y - center;
+				double rs = sqrt(rx * rx + ry * ry);
+				rx = j - center;
+				ry = i - center;
+				double rt = sqrt(rx * rx + ry * ry);
+
+				if (rs < radius && rt < radius)
+					continue;
+				if (rs >= radius && rt >= radius)
+					continue;
+				
+				p += norm(ee)
+					* norm(ee)
+					* it->dt
+					/ (2.0 * pow(ee.dot(ee), 3.0 / 2.0));
+			}
+		}
 	}
 
 	return p;
@@ -58,6 +90,7 @@ int main(int argc, char *argv[])
 {
 	Mat image;
 	image = imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
+	double radius = atof(argv[2]);
 
 	Neighborhood neigh;
 	/* Size 4 */
@@ -67,6 +100,7 @@ int main(int argc, char *argv[])
 	neigh.add( 0,-1, 1.0);
 	neigh.setupAngles();
 
+	cout << "Size 4: " << per(neigh, radius) << endl;
 	cout << "Size 4: " << per(neigh, image) << endl;
 	cout << "Size 4: " << per(neigh, image) / M_PI << endl;
 
@@ -77,6 +111,7 @@ int main(int argc, char *argv[])
 	neigh.add(-1,-1, 1.0);
 	neigh.setupAngles();
 
+	cout << "Size 8: " << per(neigh, radius) << endl;
 	cout << "Size 8: " << per(neigh, image) << endl;
 	cout << "Size 8: " << per(neigh, image) / M_PI << endl;
 
@@ -84,6 +119,7 @@ int main(int argc, char *argv[])
 	neigh.add8(1, 2, 1.0);
 	neigh.setupAngles();
 
+	cout << "Size 16: " << per(neigh, radius) << endl;
 	cout << "Size 16: " << per(neigh, image) << endl;
 	cout << "Size 16: " << per(neigh, image) / M_PI << endl;
 
@@ -92,6 +128,7 @@ int main(int argc, char *argv[])
 	neigh.add8(3, 2, 1.0);
 	neigh.setupAngles();
 
+	cout << "Size 32: " << per(neigh, radius) << endl;
 	cout << "Size 32: " << per(neigh, image) << endl;
 	cout << "Size 32: " << per(neigh, image) / M_PI << endl;
 
@@ -100,6 +137,7 @@ int main(int argc, char *argv[])
 	neigh.add8(3, 4, 1.0);
 	neigh.setupAngles();
 
+	cout << "Size 48: " << per(neigh, radius) << endl;
 	cout << "Size 48: " << per(neigh, image) << endl;
 	cout << "Size 48: " << per(neigh, image) / M_PI << endl;
 
@@ -109,6 +147,7 @@ int main(int argc, char *argv[])
 	neigh.add8(3, 5, 1.0);
 	neigh.setupAngles();
 
+	cout << "Size 72: " << per(neigh, radius) << endl;
 	cout << "Size 72: " << per(neigh, image) << endl;
 	cout << "Size 72: " << per(neigh, image) / M_PI << endl;
 
